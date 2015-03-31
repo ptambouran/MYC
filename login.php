b3a8cbdfa8602dc4e86fa9d6f296a50a0c4798d2
ptambouran <pierre.tambouran-moutoumodely@isep.fr>
2015-03-31 21:57:49 +0200

    Formulaire  Centré
    Ajout de Formulaire

---
+++ login.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="login.css" rel="stylesheet" type="text/css">
+<title>Document sans nom</title>
+</head>
+
+<body>
+
+<div> 
+<section>
+<form class="form" post="method" action="file:///D|/TOP/IslandProject/IslandProject/inscriptions.php">
+  <p>
+    <label for="nom">Nom :</label>
+    <input type="text" id="nom" /><br />
+    <label for="prenom">Prénom :</label>
+    <input type="text" id="prenom" /><br />
+    <label for="mdp">Mot de passe :</label>
+    <input type="password" id="mdp" /> <br/>
+    <label for="mdp">Confirmez votre mot de passe :</label>
+    <input type="password" id="mdp" /><br/>
+        <label for="email"> E-Mail </label>
+    <input type="email" id="email" /><br/><br/>
+    
+    
+    
+    <input class="bouton" type="submit" value="Validez"> <br/>
+  </p>
+</form>
+
+
+
+
+
+
+       </section>
+</div>
+</body>
+</htm>
